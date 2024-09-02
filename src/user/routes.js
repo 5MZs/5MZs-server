0f3f5b9b8798e0938e0e4513e876863f65886cb5
choi14123 <ckdghks5565@gmail.com>
2024-09-02 21:08:02 +0900

    [FEAT] card info api

--- src/user/routes.js
+++ src/user/routes.js
@@ -4,5 +4,6 @@ const controller = require("./controller");
 const router = Router();
 
 router.get("/", controller.getUsers);
+router.get("/get/cardinfo", controller.getCardBenefitsInfo);
 
 module.exports = router;
