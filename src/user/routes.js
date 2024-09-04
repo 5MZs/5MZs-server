a09b0e69e18946e3c3077238c23d8b4eb3c30db3
choi14123 <ckdghks5565@gmail.com>
2024-09-04 17:18:34 +0900

    [FEAT] login 토큰 기능 추가

--- src/user/routes.js
+++ src/user/routes.js
@@ -7,4 +7,24 @@ router.get("/api/v1/users", controller.getUsers);
 router.post("/join", controller.postJoin);
 router.post("/login", controller.postLogin);
 
+router.get('/protected', (req, res) => {
+  // 요청 헤더에서 토큰 추출
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(403).json({ message: 'No token provided.' });
+  }
+
+  // 토큰 검증 및 디코딩
+  jwt.verify(token, SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Invalid token.' });
+    }
+
+    // 성공적으로 토큰이 검증되면 user_id 추출
+    const user_id = decoded.user_id;
+    res.json({ user_id });
+  });
+});
+
 module.exports = router;
