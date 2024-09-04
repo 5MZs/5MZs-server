0586fb3773f887b88caeda7517bfef5f47725bf3
Choi Chang Hwan <63833865+choi14123@users.noreply.github.com>
2024-09-04 10:47:04 +0900

    [FEAT] 로그인 기능 공백 제거 추가 (#12)

--- src/user/controller.js
+++ src/user/controller.js
@@ -1,6 +1,5 @@
 const pool = require('../../db');
 const queries = require('../user/queries');
-const bcrypt = require('bcrypt');
 
 const getUsers = (req, res) => {
   pool.query(queries.getUsers, (error, results) => {
@@ -34,16 +33,14 @@ const postLogin = async (req, res,
     if (result.rowCount === 0) {
       return res.status(401).json({ error: '사용자 ID 또는 비밀번호가 잘못되었습니다.' });
     }
-    const match = bcrypt.compare(String(password), String(result.rows[0].password));
-    if (match) {
+    const match = String(password).trim() === String(result.rows[0].password).trim();
+      if (match) {
+        res.status(200).json({ message: '로그인 성공!' });
+      } else {
 
-      res.status(200).json({ message: '로그인 성공!' });
-    } else {
-
-      res.status(401).json({ error: '사용자 ID 또는 비밀번호가 잘못되었습니다.' });
+        res.status(401).json({ error: '사용자 ID 또는 비밀번호가 잘못되었습니다.' });
     }
   } catch (error) {
-
     console.error('Error during login:', error);
     res.status(500).json({ error: '서버 오류. 나중에 다시 시도해 주세요.' });
   }
