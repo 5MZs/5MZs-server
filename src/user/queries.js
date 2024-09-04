53f892daa3fca88ad408586b3ece1a1e2b493754
choi14123 <ckdghks5565@gmail.com>
2024-09-04 09:18:10 +0900

    [FEAT] login 기능 구현

--- src/user/queries.js
+++ src/user/queries.js
@@ -1,9 +1,11 @@
 const getUsers = "SELECT * FROM users";
 const getUsersUserId = "SELECT user_id FROM public.users";
-const postUsers = "INSERT INTO users (user_id, password, phone_number, nick_name, card_no) VALUES ($1, $2, $3, $4, $5)";
+const insertQuery = 'INSERT INTO users (user_id, password, phone_number, nick_name, card_no) VALUES ($1, $2, $3, $4, $5)';
+const checkQuery = 'SELECT * FROM users WHERE user_id = $1';
 
 module.exports = {
   getUsers,
-  postUsers,
   getUsersUserId,
+  insertQuery,
+  checkQuery,
 }
