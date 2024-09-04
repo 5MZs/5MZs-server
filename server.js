56f075826b56b0e74f2d4453202b53e1d30b62d9
Choi Chang Hwan <63833865+choi14123@users.noreply.github.com>
2024-09-04 17:29:19 +0900

    [FEAT] login 토큰 기능 추가 (#17)

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const jwt = require('jsonwebtoken');
+
 const userRoutes = require("./src/user/routes");
 const cardBenefitsInfo = require("./src/cardbenefitsinfo/routes");
 const mapInfo = require("./src/mapInfo/routes");
--- src/user/controller.js
+++ src/user/controller.js
@@ -1,5 +1,7 @@
 const pool = require('../../db');
 const queries = require('../user/queries');
+const SECRET_KEY = require("../../.env");
+const jwt = require('jsonwebtoken');
 
 // 사용자 리스트 가져오기
 const getUsers = (req, res) => {
@@ -34,10 +36,11 @@ const postJoin = async (req, res, next) => {
 };
 
 // 로그인 처리
-const postLogin = async (req, res, next) => {
+const postLogin = async (req, res) => {
   const { user_id, password } = req.body;
 
   try {
+    // 데이터베이스에서 사용자 검증
     const result = await pool.query(queries.checkQuery, [user_id]);
 
     // 사용자 ID 존재 여부 확인
@@ -50,15 +53,19 @@ const postLogin = async (req, res, next) => {
     const inputPassword = String(password).trim();
 
     if (inputPassword === storedPassword) {
-      res.status(200).json({ message: '로그인 성공!' });
+      // JWT 생성
+      const token = jwt.sign({ user_id: user_id }, SECRET_KEY, { expiresIn: '1h' });
+      
+      return res.status(200).json({ message: '로그인 성공!', token});
     } else {
-      res.status(401).json({ error: '사용자 ID 또는 비밀번호가 잘못되었습니다.' });
+      return res.status(401).json({ error: '사용자 ID 또는 비밀번호가 잘못되었습니다.' });
     }
   } catch (error) {
     console.error('Error during login:', error);
-    res.status(500).json({ error: '서버 오류. 나중에 다시 시도해 주세요.' });
+    return res.status(500).json({ error: '서버 오류. 나중에 다시 시도해 주세요.' });
   }
 };
+
 
 module.exports = {
   getUsers,
